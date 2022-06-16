72f030b74c7b7e94e79d46b7f21d243fb550a8e5
Jonathan DURAND <jonathan.durand@anteagroup.fr>
2022-06-16 05:56:43 +0200

    fix indent

--- geo-layer/geo-layer.js
+++ geo-layer/geo-layer.js
@@ -25,34 +25,34 @@ L.Map.addInitHook(function () {
 // Based on https://enketo.github.io/enketo-core/tutorial-40-widgets.html
 export default class GeoLayer extends Widget {
 
-	static get selector() {
+    static get selector() {
         return '.question.or-appearance-geo-layer input[data-type-xml="geopoint"]:not([data-setgeopoint]), .question.or-appearance-geo-layer input[data-type-xml="geotrace"], .question.or-appearance-geo-layer input[data-type-xml="geoshape"]';
     }
 
-	_init() {
-		const that = this;
+    _init() {
+        const that = this;
         const img = this.question.querySelector( 'img' );
-		this.permanentLabel = this.props.appearances.length && this.props.appearances.includes("geo-layer-permanentlabel");
-		this.tooltipDirection = 'top';
-		if ( !img ) {
+        this.permanentLabel = this.props.appearances.length && this.props.appearances.includes("geo-layer-permanentlabel");
+        this.tooltipDirection = 'top';
+        if ( !img ) {
             this._showGeoJsonError( 'geojsonNotFound' );
         } else if ( img.getAttribute( 'src' ) ) {
             // This sleep is needed for waiting the initialization of geomap. can be make a different solution ?
-			this.sleep(2000).then(()=>{
+            this.sleep(2000).then(()=>{
             return this._addMarkup( img )
-				.then( this._addFunctionality.bind( this ) )
+                .then( this._addFunctionality.bind( this ) )
                 .then( () => this );
-			});
+            });
         } else {
-			console.error("Offline mode not implemented");
-		}
+            console.error("Offline mode not implemented");
+        }
     }
 
-	sleep(ms) {
-	  return new Promise(resolve => setTimeout(resolve, ms));
-	}
+    sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
-	/**
+    /**
      * Zoom to the extend of selectable features
      * Only if you add geojson-map-zoom-extend appearance
      */
@@ -72,15 +72,15 @@ export default class GeoLayer extends Widget {
             }
         }
     }
-	
-	/**
+    
+    /**
      * @param {Element} img - the image element
      * @return {Promise} the widget element
      */
     _addMarkup( img ) {
         const that = this;
         const src = img.getAttribute( 'src' );
-		that.question.querySelectorAll( 'img' ).forEach( el => el.remove() );
+        that.question.querySelectorAll( 'img' ).forEach( el => el.remove() );
         /**
          * For translated forms, we now discard everything except the first image,
          * since we're assuming the images will be the same in all languages.
@@ -89,31 +89,31 @@ export default class GeoLayer extends Widget {
             .then( response => response.json() )
             .then( geojson => {
                 if(that._isValidGeojson(geojson)){
-					that.map = this.question.querySelector( '.map-canvas-wrapper .map-canvas' )._leaflet_map;
+                    that.map = this.question.querySelector( '.map-canvas-wrapper .map-canvas' )._leaflet_map;
                     that.geoJsonLayer = that._createLayer(geojson);
                 }
             })
-			.catch( (error) => {
-				that._showGeoJsonError(error);
-			})
+            .catch( (error) => {
+                that._showGeoJsonError(error);
+            })
     }
 
-	/**
+    /**
      * @param {object} widget - the widget element
      */
     _addFunctionality( widget ) {
         // widget param is a copy from image-map - here, is ever null. What is it for in image-map ?
-		const that = this;
+        const that = this;
         that._zoomToExtend();
-		that.geoJsonLayer.eachLayer(function(layer) {
-			if (layer instanceof L.Marker){
-				layer.getElement().style.color =DEFAULT_COLOR;          
-			}
-		});
-		that.geoJsonLayer.off('click');
+        that.geoJsonLayer.eachLayer(function(layer) {
+            if (layer instanceof L.Marker){
+                layer.getElement().style.color =DEFAULT_COLOR;          
+            }
+        });
+        that.geoJsonLayer.off('click');
     }
-	
-	/**
+    
+    /**
      * Create a geojson layer and added to the map
      * @param {json} A valid geoJSON
      * @return {object} A leaflet layer
@@ -127,45 +127,45 @@ export default class GeoLayer extends Widget {
         const geojsonLayer = L.geoJSON(geojson["features"], {
             onEachFeature: function (feature, layer) {
                 const name = feature["properties"]["name"];
-				const label = feature["properties"]["label"];
+                const label = feature["properties"]["label"];
                 if(name){ // enable click action only on feature who has a name match with the list (_removeUnmatchedIds)
-					let tooltip = name;
-					if ( label ) {
-						tooltip = label;
-					}
-					const classes = `geojson-label geojson-${feature.geometry.type} ${that.instanceName}`
-					layer.bindTooltip(tooltip, {permanent: that.permanentLabel, direction:that.tooltipDirection, className: classes})
-					
+                    let tooltip = name;
+                    if ( label ) {
+                        tooltip = label;
+                    }
+                    const classes = `geojson-label geojson-${feature.geometry.type} ${that.instanceName}`
+                    layer.bindTooltip(tooltip, {permanent: that.permanentLabel, direction:that.tooltipDirection, className: classes})
+                    
                 }
             },
             pointToLayer: function (feature, latlng) {
                 return L.marker(latlng, {
                     icon: DEFAULT_ICON,
-					clickable: false // todo not work ?
+                    clickable: false // todo not work ?
                     // title: "TODO name"
                 });
             }
         }).addTo(that.map);
-		
+        
         // This layer is used when appearance geojson-map-zoom-extend is set (for zoom on)
         that.layerExtend = new L.GeoJSON(geojson["features"], {
             filter: function(feature, layer) {
                 return feature.properties.name;
             }
         });
-		if ( that.tooltipMinZoom ) {
-			that.map.on('zoomend', function() {				
-			  if (that.map.getZoom() < that.tooltipMinZoom) {
-				  that.map.getPane('tooltipPane').style.display = 'none';
-			  } else {
-				  that.map.getPane('tooltipPane').style.display = 'block';
-			  }
-			})
-		}
+        if ( that.tooltipMinZoom ) {
+            that.map.on('zoomend', function() {                
+              if (that.map.getZoom() < that.tooltipMinZoom) {
+                  that.map.getPane('tooltipPane').style.display = 'none';
+              } else {
+                  that.map.getPane('tooltipPane').style.display = 'block';
+              }
+            })
+        }
         return geojsonLayer;
     }
-	
-	/**
+    
+    /**
      * Test is GeoJSON is valid
      *
      * @param {object} data - an json
@@ -178,8 +178,8 @@ export default class GeoLayer extends Widget {
         // TODO : test foreach feature the type, properties and geometrie ?
         return true;
     }
-	
-	/**
+    
+    /**
      * @param {Error} err - error message
      */
     _showGeoJsonError( err ) {
@@ -190,30 +190,30 @@ export default class GeoLayer extends Widget {
             </div>`
         );
         this.question.querySelector( '.geopicker.widget' ).before( fragment );
-		this.question.querySelector( '.geopicker.widget' ).style.display = 'block';
+        this.question.querySelector( '.geopicker.widget' ).style.display = 'block';
     }
-	 
-	 
+     
+     
     /*
      * If you want overrides some Widget methods
     disable() {
         //this.disabled = true;
     }
-	
+    
     enable() {
         //this.disabled = false;
     }
-	
+    
     update() {
         //this.value = this.originalInputValue;
     }
-	
+    
     get value() {
         //return this.element.classList.contains( 'empty' ) ? '' : this.value;
     }
-	
+    
     set value( value ) {
         //this.value = value;
     }
-	*/
+    */
 }
